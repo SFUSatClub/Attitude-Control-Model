68c024e7b7b12aecf58e412f99940bbef27362ba
HarrisonHandley <Harrisonlhandley@gmail.com>
2017-02-25 23:01:28 -0800

    Vector Field of Earths Magnetic Field above CHIME

--- Orbital_Model.m
+++ Orbital_Model.m
@@ -1,4 +1,4 @@
-function [ output_args ] = Orbital_Model( Time, eccentricity, inclination, semimajor_axis, True_Anomaly, Interval)
+function [ output_args ] = Orbital_Model( Year, Month, Day, Hour, Minutes, Seconds, eccentricity, inclination, semimajor_axis, True_Anomaly, Interval)
 % Orbital Model Summary
 %   The orbit of the satellite is defined in Earth-Centered Inertial
 %   Reference Frame and requires converting into ECEF to provide
@@ -19,26 +19,34 @@ function [ output_args ] = Orbital_Model( Time, eccentricity, inclination, semim
 %   Constants
 GM = 3.986005*10^14;
 
-
-semiminor_axis = sqrt(semimajor_axis^2-(eccentricity*semimajor_axis)^2);
+semiminor_axis = sqrt((semimajor_axis^2)-(eccentricity*semimajor_axis)^2);
 Total_Area = pi*semiminor_axis*semimajor_axis;
 Orbital_Period=sqrt((4*(pi^2)*(semimajor_axis)^3)/(GM));
 
 Area_per_second_travelled = Total_Area/Orbital_Period;
 
 %   Find Area of the orbit so far from current true anamoaly
-Current_Eccentric_Anomaly = 2*arctan(sqrt((1-eccentricity)/(1+eccentricity))*tan(True_anaomaly/2));
+Current_Eccentric_Anomaly = 2*atan(sqrt((1-eccentricity)/(1+eccentricity))*tan(True_anaomaly/2));
 True_Anamoly_Area = 0.5*semimajor_axis*semiminor_axis*(Current_Eccentricity_Anamoaly-eccentricity*sin(Current_Eccentricity_Anamoly));
 
 %   Find new area and subsequently new true anamoaly
 New_Area = True_Anamoaly_Area + Area_per_second*interval;
-if New_Area > Total_Area
-    New_Area = New_Area - Total_Area;
-end
+    if New_Area > Total_Area
+       New_Area = New_Area - Total_Area;
+    end
 
+%   Find the new true_anomaly that correlates with the New Area covered by the
+%   orbit
 fun = x*(1-eccentricity*sin(x))-(2*New_Area/(semimajor_axis*semiminor_axis));
-New_Eccentric_Anomaly = fzero(fun, x0);
+New_Eccentric_Anomaly = fzero(fun, Current_Eccentric_Anomaly);
+fun2 = 2*atan((sqrt((1-eccentricity)/(1+eccentricity))*tan(x/2)))-New_Eccentric_Anomaly;
+New_True_Anomaly = fzero(fun2, True_Anomaly);
 
+%   Calculate the transformation matrix for ECI to ECEF using IAU-76/FK5 as
+%   reduction methodology
 
+ECI2ECEF_Transformation_Matrix = dcmeci2ecef('IAU-76/FK5',[Year Month Day Hour Minutes Seconds]);
+
+%   Calculate ECI coordinates
 end
 
