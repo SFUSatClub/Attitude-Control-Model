a3e195adcb3fdc21b0f2424830040a03dca2e4f6
TobiNakamura <tobishires@gmail.com>
2017-07-18 04:36:46 -0700

    general spacraft property configuration entry via ini file

--- GUI.m
+++ GUI.m
@@ -22,7 +22,7 @@ function varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-% Last Modified by GUIDE v2.5 18-Jul-2017 03:50:16
+% Last Modified by GUIDE v2.5 18-Jul-2017 04:15:11
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -280,7 +280,8 @@ RAAN = str2double(get(handles.RAAN_Input, 'string'));
 Arg_of_Perigee = str2double(get(handles.Arg_of_Perigee_Input, 'string'));
 Num_of_Orbits = str2double(get(handles.Num_of_Orbits_Input, 'string'));
 Magnetic_Moment = [1; 1; 1];
-Results = Model_Executor(Launch_Date, Eccentricity, Inclination, Semimajor_Axis, Orbital_Interval, RAAN, Arg_of_Perigee, Num_of_Orbits, Magnetic_Moment);
+Spacecraft_Config = get(handles.Config_File, 'string');
+Results = Model_Executor(Launch_Date, Eccentricity, Inclination, Semimajor_Axis, Orbital_Interval, RAAN, Arg_of_Perigee, Num_of_Orbits, Magnetic_Moment, Spacecraft_Config);
 assignin('base', 'Results', Results);
 axes(handles.Main_Plot)
 
@@ -456,13 +457,15 @@ end
 
 % --- Executes on button press in Load_Config.
 function Load_Config_Callback(hObject, eventdata, handles)
-[FileName, PathName] = uigetfile('*.ini', 'Select Model Configuration File')
+[Config_FileName, Config_PathName] = uigetfile('*.ini', 'Select Model Configuration File');
 ini = IniConfig();
-ini.ReadFile(strcat(PathName, FileName))
+if ~ini.ReadFile(strcat(Config_PathName, Config_FileName))
+    disp(strcat('Could not read Config File: ', Config_FileName))
+end;
 % sections = ini.GetSections()
 % keys = ini.GetKeys(sections{1})
 % values = ini.GetValues(sections{1}, keys)
-
+set(handles.Config_File, 'string', strcat(Config_PathName, Config_FileName));
 set(handles.Orbital_Interval_Input, 'string', num2str(ini.GetValues('Model', 'Orbit_Interval')));
 set(handles.Num_of_Orbits_Input, 'string', num2str(ini.GetValues('Model', 'Num_Orbits')));
 set(handles.Eccentricity_Input, 'string', num2str(ini.GetValues('Orbit', 'Eccentricity')));
@@ -473,3 +476,49 @@ set(handles.Arg_of_Perigee_Input, 'string', num2str(ini.GetValues('Orbit', 'Arg_
 % hObject    handle to Load_Config (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+
+
+function edit11_Callback(hObject, eventdata, handles)
+% hObject    handle to edit11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit11 as text
+%        str2double(get(hObject,'String')) returns contents of edit11 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit11_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit11 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function Config_File_Callback(hObject, eventdata, handles)
+% hObject    handle to Config_File (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of Config_File as text
+%        str2double(get(hObject,'String')) returns contents of Config_File as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function Config_File_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to Config_File (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
